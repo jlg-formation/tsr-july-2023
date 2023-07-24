a7e5058fa8eacbec74570a41a36050653aed8aac
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-07-24 17:22:26 +0200

    dev: added explicit typings

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,14 +1,16 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import serveIndex from "serve-index";
 import api from "./api";
 
 const app = express();
 const port = 3000;
 
-app.use((req, res, next) => {
+const logger = (req: Request, res: Response, next: NextFunction) => {
   console.log("req: ", req.method, req.url);
   next();
-});
+};
+
+app.use(logger);
 
 app.use("/api", api);
 
