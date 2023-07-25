f49cc1f3b4a13e2d7fda9e1f8f3615395acf4e0a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-07-25 17:13:12 +0200

    dev: added cypress test

--- test/cypress/e2e/spec.cy.ts
+++ test/cypress/e2e/spec.cy.ts
@@ -1,5 +1,19 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("template spec", () => {
+  it("passes", () => {
+    cy.visit("http://localhost:5173");
+
+    cy.intercept("http://localhost:3000/api/random-config", {
+      samples: 17,
+      multiplicationFactor: 51,
+    });
+
+    cy.contains("Multiplication");
+    cy.contains("samples", { matchCase: false });
+    cy.contains("label", "Multiplication Factor");
+    cy.contains("button", "Get Config").click();
+    cy.contains("label span.value", "17");
+
+    cy.get(".samples input[type=range]").invoke("val", 45).trigger("input");
+    cy.contains("label span.value", "45");
+  });
+});
