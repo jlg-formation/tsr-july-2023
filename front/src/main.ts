ed7bd0ad956e89b266cb45ea24876a97f444fd84
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-07-25 11:50:18 +0200

    dev: added lines

--- front/src/main.ts
+++ front/src/main.ts
@@ -5,8 +5,8 @@ const cx0 = 50;
 const cy0 = 50;
 const r0 = 45;
 
-const samples = 10;
-const multiplicationFactor = 2;
+const samples = 100;
+const multiplicationFactor = 98;
 
 const svgns = "http://www.w3.org/2000/svg";
 const container = document.querySelector("svg g.samples");
@@ -26,4 +26,26 @@ for (let i = 0; i < samples; i++) {
   circle.setAttributeNS(null, "cy", y + "");
   circle.setAttributeNS(null, "r", "1");
   container.appendChild(circle);
+}
+
+const lineContainer = document.querySelector("svg g.lines");
+if (lineContainer === null) {
+  throw new Error("Cannot find selector svg g.lines");
+}
+for (let i = 0; i < samples; i++) {
+  const line = document.createElementNS(svgns, "line");
+
+  const angle1 = (i * (Math.PI * 2)) / samples;
+  const angle2 = (i * multiplicationFactor * (Math.PI * 2)) / samples;
+
+  const x1 = cx0 + Math.cos(angle1) * r0;
+  const y1 = cy0 + Math.sin(angle1) * r0;
+  const x2 = cx0 + Math.cos(angle2) * r0;
+  const y2 = cy0 + Math.sin(angle2) * r0;
+
+  line.setAttributeNS(null, "x1", x1 + "");
+  line.setAttributeNS(null, "y1", y1 + "");
+  line.setAttributeNS(null, "x2", x2 + "");
+  line.setAttributeNS(null, "y2", y2 + "");
+  lineContainer.appendChild(line);
 }
